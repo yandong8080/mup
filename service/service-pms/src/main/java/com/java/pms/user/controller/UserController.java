4d9f00198473605f326b209785b3989751246983
LiuYandong <yandong8080@163.com>
2020-10-11 16:38:51 +0800

    项目初始化(新增UserController)

---
+++ service/service-pms/src/main/java/com/java/pms/user/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.java.pms.user.controller;
+
+import com.java.commonutils.api.APICODE;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/pms/user")
+@CrossOrigin
+public class UserController {
+    @PostMapping("login")
+    public APICODE login() {
+        return APICODE.OK().data("token", "admin");
+    }
+
+    @GetMapping("info")
+    public APICODE info() {
+        return APICODE.OK().data("roles", "[admin]").data("name", "admin").data("avatar", "http://img0.imgtn.bdimg.com/it/u=1782959667,617309577&fm=26&gp=0.jpg");
+    }
+
+
+}
