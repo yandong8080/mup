f051f6188086ce077fa21063a328a1f581fff3e7
LiuYandong <yandong8080@163.com>
2020-10-15 09:52:41 +0800

    POI Excel 导入导出

--- service/service-pms/src/main/java/com/java/pms/dispord/controller/DispOrdController.java
+++ service/service-pms/src/main/java/com/java/pms/dispord/controller/DispOrdController.java
@@ -1,20 +1,30 @@
 package com.java.pms.dispord.controller;
 
+import com.alibaba.excel.EasyExcel;
 import com.java.pms.dispord.entity.DispOrd;
+import com.java.pms.dispord.listener.ExcelListener;
 import com.java.pms.dispord.service.DispOrdService;
 import com.java.commonutils.api.APICODE;
+import com.java.pms.dispord.vo.DispOrdData;
 import com.java.pms.dispord.vo.DispOrdQuery;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.List;
 
 @Api(tags = "调度指令库管理 - 控制层")
 @RestController
 @RequestMapping("/pms/dispord")
+@CrossOrigin //解决跨域问题
 public class DispOrdController {
 
     @Autowired
@@ -35,7 +45,7 @@ public class DispOrdController {
 
     @ApiOperation(value = "带条件的分页查询")
     @PostMapping("{pageNo}/{pageSize}")
-    public APICODE findPageDispOrd(DispOrdQuery dispOrdQuery, @PathVariable(name = "pageNo") int pageNo, @PathVariable(name = "pageSize") int pageSize){
+    public APICODE findPageDispOrd(@RequestBody(required = false)DispOrdQuery dispOrdQuery, @PathVariable(name = "pageNo") int pageNo, @PathVariable(name = "pageSize") int pageSize){
         Page<DispOrd> page = dispOrdService.findPageDispOed(dispOrdQuery, pageNo, pageSize);
         long totalElements = page.getTotalElements();
         List<DispOrd> dispOrdList = page.getContent();
@@ -68,6 +78,59 @@ public class DispOrdController {
     public APICODE deleteDispOrdById(@PathVariable(name = "dispOrdId") String dispOrdId) {
         dispOrdService.removeById(dispOrdId);
         return APICODE.OK();
+    }
+
+    @ApiOperation(value = "列表导出", notes = "使用阿里EasyExcel导出Excel格式的列表数据")
+    @GetMapping("exportEasyExcel")
+    public void exportEasyExcel(HttpServletResponse response) {
+        // ## 查询调度指令库
+        List<DispOrd> dispOrdList = dispOrdService.findAll();
+        // ## 定义导出列表集合
+        List<DispOrdData> dispOrdDataList = new ArrayList<>();
+
+        for (DispOrd dispOrd : dispOrdList) {
+            // 创建封装类
+            DispOrdData dispOrdData = new DispOrdData();
+            // copy数据
+            BeanUtils.copyProperties(dispOrd, dispOrdData);
+            //添加对象到集合中
+            dispOrdDataList.add(dispOrdData);
+        }
+        //导出 Excel
+        String fileName = "dispOrdList";
+
+        try {
+            // ## 设置响应信息
+            response.reset();
+            response.setContentType("application/vnd.ms-excel; charset=utf-8");
+            response.setCharacterEncoding("utf-8");
+            response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "utf-8") + ".xlsx");
+            EasyExcel.write(response.getOutputStream(), DispOrdData.class).sheet("指令列表").doWrite(dispOrdDataList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @ApiOperation(value = "列表导入", notes = "使用阿里EasyExcel导入Excel格式的列表数据")
+    @PostMapping("importEasyExcel")
+    public void importEasyExcel(MultipartFile file) {
+        try {
+            EasyExcel.read(file.getInputStream(), DispOrdData.class, new ExcelListener(dispOrdService)).sheet().doRead();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @ApiOperation(value = "列表导出", notes = "使用POI导出Excel格式的列表数据")
+    @GetMapping("exportExcel")
+    public void exportExcel(HttpServletResponse response) {
+        dispOrdService.exportExcel(response);
+    }
+
+    @ApiOperation(value = "列表导入", notes = "使用POI导入Excel格式的列表数据")
+    @PostMapping("importExcel")
+    public void importExcel(MultipartFile file) {
+        dispOrdService.importExcel(file);
     }
 
 }
