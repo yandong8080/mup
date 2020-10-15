4237c9e9b311a7e3c9590c752367f7869adee49e
LiuYandong <yandong8080@163.com>
2020-10-15 08:44:04 +0800

    设置Excel表头和添加的数据字段

---
+++ service/service-pms/src/main/java/com/java/pms/dispord/vo/DispOrdData.java
@@ -0,0 +1,25 @@
+package com.java.pms.dispord.vo;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+import lombok.Data;
+
+/**
+ * 设置表头和添加的数据字段
+ */
+@Data
+public class DispOrdData {
+
+    @ExcelProperty(value = "指令名称", index = 0)
+    private String orderName;
+
+    @ExcelProperty(value = "指令类型", index = 1)
+    private int specType;
+
+    @ExcelProperty(value = "优先级", index = 2)
+    private int priority;
+
+    @ExcelProperty(value = "指令描述", index = 3)
+    private String orderDesc;
+
+
+}
